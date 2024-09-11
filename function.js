dffabbce982f54599c29c6cc772d7324cdd2ecb3
SzarkaNorbi <norbertszarkakovacs@gmail.com>
2024-09-11 19:24:52 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+function addNumbers() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var sum = num1 + num2;
+    
+    document.getElementById('result').innerText = 'Az eredmény: ' + sum;
+  }
